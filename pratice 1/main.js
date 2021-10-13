bd2dbee7c7c962133511e3928e1acb432263f68f
jalbiston <jacob.albiston@gmail.com>
2021-10-12 19:54:59 -0600

    square foot calculator

---
+++ pratice 1/main.js
@@ -0,0 +1,36 @@
+// function floor(diameter) {
+//   var radius = diameter/2;
+//   var floorArea = Math.PI * (radius * radius);
+//   return floorArea;
+// }
+
+// function walls(diameter, height) {
+//   // var diameter = 10;
+//   // var height = 8;
+//   var radius = diameter / 2;
+//   var circumference = 2 * Math.PI * radius;
+//   var wallArea = circumference * height;
+//   return wallArea
+// }
+
+
+// function materialsNeeded(){
+//   var d = document.getElementById("across").value;
+//   var h = document.getElementById("height").value;
+
+//   var carpetNeeded = Math.ceil(floor(d));
+//   var paintNeeded = Math.ceil(walls(d, h));
+//   console.log('carpet needed is '+carpetNeeded+ ' sqft')
+//   console.log('Paint Needed is '+paintNeeded+ ' sqft')
+
+
+
+// } 
+
+
+
+var i = 1;
+while (i<=100) {
+  console.log(i);
+  i=i+5
+}
